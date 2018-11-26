43c9844e2007c2f358471d61e87251e5fc0ee5de
unknown <adrien_1996@hotmail.com>
2018-11-25 22:56:31 -0600

    added ability to add pages

---
+++ forumcreate.php
@@ -0,0 +1,36 @@
+<?php
+    require('connect.php');
+    session_start();
+    if(!isset($_SESSION['type']))
+    {
+        header('Location: index.php');
+    }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Admin Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+    <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
+    <script>tinymce.init({ selector:'textarea' });</script>
+</head>
+<body>
+<!-- a forum will be created and will have a title, Description, createdDate, Rules, updatedDate -->
+    <a href="index.php">Go Home</a>
+    <form id="forum"
+		action="forumcreating.php"
+        method="post">
+        <label for="title">Forum Title</label>
+        <input id="title" name="title" type="text">
+        <label for="description">Forum Description</label>
+        <textarea name="description" id="description" cols="30" rows="10"></textarea>
+        <label for="rules">Forum Rules</label>
+        <textarea name="rules" id="rules" cols="30" rows="10"></textarea>
+        <button type="submit" value="Submit">Submit</button>
+    </form>
+</body>
+</html>
---
+++ forumcreating.php
@@ -0,0 +1,28 @@
+<?php
+    require('connect.php');
+    session_start();
+    if(!isset($_SESSION['type']))
+    {
+        header('Location: index.php');
+    }
+    $title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $description = $_POST['description'];
+    $rules = $_POST['rules'];
+
+    date_default_timezone_set('America/Winnipeg');
+    $date = date('Y-m-d');
+
+    $username = $_SESSION['username'];
+
+    $insertQuery = "INSERT INTO forums (title,description,rules,createdDate,username) VALUES (:title,:description,:rules,:createdDate,:username)";
+    $insertStatement = $db-> prepare($insertQuery);
+    $insertStatement->bindValue(':title',$title);
+    $insertStatement->bindValue(':description',$description);
+    $insertStatement->bindValue(':rules',$rules);
+    $insertStatement->bindValue(':createdDate',$date);
+    $insertStatement->bindValue(':username',$username);
+    $insertStatement->execute();
+    header('Location: index.php');
+    //what could i ever possibly do to turn this into a user page
+    //header('Location: adminforumlist.php?orderBy=title');
+?>
